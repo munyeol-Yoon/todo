12c9b0e54be8d4ef3efeb9dcfee520fa0705a4c2
muyeon <gmakin36@gmail.com>
2024-05-14 23:36:00 +0900

    style: add todo form button style - todo form 버튼 스타일 추가

--- src/components/todos/buttons/TodoFormButton.jsx
+++ src/components/todos/buttons/TodoFormButton.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../../../styles/index.css";
 
 const TodoFormButton = () => {
-  return <button type="submit">추가하기</button>;
+  return (
+    <button type="submit" className="todo-form-button">
+      추가하기
+    </button>
+  );
 };
 
 export default TodoFormButton;
